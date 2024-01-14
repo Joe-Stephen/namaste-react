53a432405523748e4a8460e72dc753bf80a2ea38
Joe Stephen <joestephenk10@gmail.com>
2024-01-14 15:24:26 +0530

    ep:1, react core, html core

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
-const heading = React.createElement('h1', {}, "Hello World!");
+const parent = React.createElement('div', {id:"parent"},
+ React.createElement('div', {id:'child'},
+  [React.createElement('h1', {}, "Hello I am heading 1 from child div!"), React.createElement('h2', {}, "Hello I am heading 2 from child div!")]));
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+root.render(parent)
