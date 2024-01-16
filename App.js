ac49b6013718faa56c2bc3ad80503f892da84d4c
Joe Stephen <joestephenk10@gmail.com>
2024-01-16 18:00:03 +0530

    react-jsx-returns-component(first impression)

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div', {id:"parent"},
- React.createElement('div', {id:'child'},
-  [React.createElement('h1', {}, "Hello I am heading 1 from child div!"), React.createElement('h2', {}, "Hello I am heading 2 from child div!")]));
+const Heading = ()=><h1>Marvel: The Avengers💫</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+const HeadingFunction = ()=>(
+  <div>
+    {Heading()}
+    <Heading />
+<Heading></Heading>
+<h1>Avengers: Infinity War</h1>
+</div>
+);
+root.render(<HeadingFunction />)
+// root.render(heading)
